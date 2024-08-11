1aba5b171953c6a678d1bd65e7529986c4a6dc24
Simon Bonnedahl <simon.bonnedahl@knowingly.co>
2024-08-11 18:42:17 +0200

    🔧 Fix auth redirects

--- apps/web/src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ apps/web/src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import {  useSearchParams } from "next/navigation";
 
 export default function SigninPage() {
   const searchParams = useSearchParams();
-  const redirect = searchParams.get("redirect");
+  const redirect = searchParams.get("redirect_url");
 
   return (
     <div className="flex items-center w-full justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
@@ -15,4 +15,3 @@ export default function SigninPage() {
     </div>
   );
 }
-  
--- apps/web/src/app/auth/sign-up/[[...sign-up]]/page.tsx
+++ apps/web/src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 
 export default function SignupPage() {
   const searchParams = useSearchParams();
-  const redirect = searchParams.get("redirect");
+  const redirect = searchParams.get("redirect_url");
   return (
     <div className="flex items-center w-full justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
       <SignUp forceRedirectUrl={redirect} />
